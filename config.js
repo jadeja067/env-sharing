94ccaeea2f2b9e2e080509b78c31e5cda20b7dc1
Kuldipsinh Jadeja <jkuldipsinh067@gmail.com>
2024-08-09 13:45:03 +0530

    Changed to new flow

--- config.js
+++ config.js
@@ -1,34 +1,9 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore, collection } from "firebase/firestore";
-// import { config } from "dotenv";
-
-// config({ path: './.env.keys'})
-// const firebaseConfig = {
-//     apiKey: process.env.APIKEY,
-//     authDomain: process.env.AUTHDOMAIN,
-//     projectId: process.env.PROJECTID,
-//     storageBucket: process.env.STORAGEBUCKET,
-//     messagingSenderId: process.env.MESSAGINGSENDERID,
-//     appId: process.env.APPID,
-//     measurementId: process.env.MEASUREMENTSID
-// };
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-// // Get a reference to the 'envs' collection
-// const envs = collection(db, 'envs');
-
-// export default envs;
-
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
 import { config } from 'dotenv';
-import fs from 'fs';
 
 config({ path: './.env.keys' });
 
-const serviceAccount = JSON.parse(fs.readFileSync('./serviceAccountKey.json', 'utf8'));
 
 admin.initializeApp({
     credential: admin.credential.cert({
--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-import { config } from 'dotenv';
 import crypto from 'crypto';
 import envs from './config.js';
-import fs, { promises } from 'fs';
-import { program } from 'commander';
-import inquirer from 'inquirer';
+import fs from 'fs';
 
-config({ path: './.env' });
-
-const algorithm = 'aes-256-ctr';
-const secretKey = process.env.ENCRYPTION_KEY;
-const iv = crypto.randomBytes(16);
-
-const registerdEnvs = JSON.parse(fs.readFileSync('./env_manager.json', 'utf8'));
-const environment_choices = Object.keys(registerdEnvs)
-
-const encrypt = (text) => {
-    const cipher = crypto.createCipheriv(algorithm, Buffer.from(secretKey), iv);
-    const encrypted = Buffer.concat([cipher.update(text), cipher.final()]);
-
-    return {
-        iv: iv.toString('hex'),
-        content: encrypted.toString('hex')
-    };
-};
-
-const writeEnvVariables = (filePath, envVars) => {
-    const envContent = Object.entries(envVars).map(([key, value]) => `${key}=${value}`).join('\n');
-    fs.writeFileSync(filePath, envContent);
-};
+const algorithm = process.env.ENCRYPTION_ALGORITHM,
+    secretKey = process.env.ENCRYPTION_KEY,
+    envFile = process.env.ENVIRONMENT_FILE_PATH;
 
 const decrypt = (hash) => {
     const decipher = crypto.createDecipheriv(algorithm, Buffer.from(secretKey), Buffer.from(hash.iv, 'hex'));
@@ -36,155 +13,19 @@ const decrypt = (hash) => {
     return decrypted.toString();
 };
 
-const loadEnvVariables = (filePath) => {
-    const envVars = {};
-    const envContent = fs.readFileSync(filePath, 'utf8');
-
-    envContent.split('\n').forEach(line => {
-        const [key, value] = line.split('=');
-        if (key && value) {
-            envVars[key.trim()] = value.trim();
-        }
-    });
-    return envVars;
+const writeEnvVariables = (filePath, envVars) => {
+    const envContent = Object.entries(envVars).map(([key, value]) => `${key}=${value}`).join('\n');
+    fs.writeFileSync(filePath, envContent);
 };
 
-program
-    .command('create')
-    .description('Create new .env instance')
-    .action(async (options) => {
-
-        try {
-            let newEnv = undefined;
-            while (!newEnv) {
-                const { option } = await inquirer.prompt({
-                    type: 'input',
-                    name: 'option',
-                    message: 'Enter name of the new environment [special chars not allowed]:',
-                    choices: environment_choices
-                });
-                if (option) {
-                    newEnv = option;
-                    break;
-                }
-            }
-            const fileValidation = await promises.access(`./.${newEnv}`, promises.constants.F_OK).catch(async (error) => {
-                await promises.writeFile(`./.${newEnv}`, '');
-            })
-
-            const envVars = loadEnvVariables(`./.${newEnv}`);
-            const encryptedData = {};
-            for (const [key, value] of Object.entries(envVars)) {
-                encryptedData[key] = encrypt(value);
-            }
-            const newDocRef = envs.doc();
-            await newDocRef.set({
-                name: newEnv,
-                data: encryptedData
-            });
-            registerdEnvs[newEnv] = {
-                id: newDocRef.id,
-                path: `./.${newEnv}`,
-            }
-            fs.writeFileSync('./env_manager.json', JSON.stringify(registerdEnvs));
-            console.log('Data added successfully');
-        } catch (error) {
-            if (error.message === "User force closed the prompt with 0 null") return
-            console.error(error);
-        }
-    });
-
-program
-    .command('push')
-    .description('Pushes updated .env to storage')
-    .option('-e, --environment <environment>', 'Specify env file')
-    .action(async (options) => {
-        try {
-            if (!options.environment) {
-                const { option } = await inquirer.prompt({
-                    type: 'list',
-                    name: 'option',
-                    message: 'Choose from registered env files.',
-                    choices: environment_choices
-                });
-                options.environment = option
-            }
-            if (!options.environment || !registerdEnvs[options.environment]) return
-
-            const envVars = loadEnvVariables(registerdEnvs[options.environment]?.path);
-            const encryptedData = {};
-
-            for (const [key, value] of Object.entries(envVars)) {
-                encryptedData[key] = encrypt(value);
-            }
-            const docRef = envs.doc(registerdEnvs[options.environment]?.id);
-            await docRef.update({
-                name: options.environment,
-                data: encryptedData
-            });
-            console.log('Data updated and encrypted successfully');
-        } catch (error) {
-            if (error.message === "User force closed the prompt with 0 null") return
-            console.error(error.message);
-        }
-    });
-
-program
-    .command('pull')
-    .description('Pulls updated .env from storage')
-    .option('-e, --environment <environment>', 'Specify env file')
-    .action(async (options) => {
-        try {
-            if (!options.environment) {
-                const { option } = await inquirer.prompt({
-                    type: 'list',
-                    name: 'option',
-                    message: 'Choose from registered env files.',
-                    choices: environment_choices
-                });
-                options.environment = option
-            } if (!registerdEnvs[options.environment]) {
-                return
-            }
-            const docRef = envs.doc(registerdEnvs[options.environment]?.id);
-            const docSnap = await docRef.get();
-
-            if (docSnap.exists) {
-                const encryptedData = docSnap.data();
-                const decryptedData = {};
-
-                for (const [key, value] of Object.entries(encryptedData.data)) {
-                    decryptedData[key] = decrypt(value);
-                }
-
-                writeEnvVariables(registerdEnvs[options.environment]?.path, decryptedData);
-                console.log('Data retrieved and written to .env file successfully');
-            } else {
-                console.log('This env is not saved in storage');
-            }
-        } catch (error) {
-            if (error.message === "User force closed the prompt with 0 null") return
-            console.error(error);
-        }
-    });
-
-program.command('refresh').action(async () => {
+const main = async () => {
     try {
-        const latestEnvData = {}
-        const snapshot = await envs.get();
-        snapshot.forEach(doc => { 
-            const id = doc.id
-            doc = doc.data()
-            latestEnvData[doc.name] = {
-                id,
-                path: `./.${doc.name}`,
-            }
-        })
-        fs.writeFileSync('./env_manager.json', JSON.stringify(latestEnvData));
+        const {docs} = await envs.get();
+        const vars = docs.map(doc => doc.data());        
+        writeEnvVariables(envFile, vars[0]);
     } catch (error) {
-        if (error.message === "User force closed the prompt with 0 null") return
         console.error(error);
     }
-})
+}
 
-program.parse(process.argv);
+main();
